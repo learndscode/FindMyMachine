48deffcac190f024588bcb724bba7683a34d0da8
Tyson Norman <tnorman75@icloud.com>
2025-07-27 13:59:19 -0500

    cleaned up display and added requirements

---
+++ geolocate.py
@@ -0,0 +1,16 @@
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+import reverse_geocoder as rg
+import pycountry
+
+def get_location(lat, lon):
+  try:
+      result = rg.search((lat, lon), mode=1)[0]
+      return pd.Series([result['name'], result['admin1'], result['cc']])
+  except Exception:
+      return pd.Series([None, None, None])
+  
+  def get_country_name(code):
+    country = pycountry.countries.get(alpha_2=code)
+    return country.name if country else code
--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import reverse_geocoder as rg
 
+from geolocate import get_location
+
 st.set_page_config(
     page_title="Find My Machines",
     page_icon="assets/SkidSteer.png",  # Optional: You can also set a page icon (favicon)
@@ -16,19 +18,32 @@ upload_file = st.file_uploader("Choose a spreadsheet file", type="xlsx")
 
 if upload_file is not None:
     # Read the Excel file into a DataFrame
-    df = pd.read_excel(upload_file, 'openpyxl')
+    df = pd.read_excel(upload_file, engine='openpyxl')
+    # Replace NaN with None across entire DataFrame
+    #df = df.apply(lambda col: col.map(lambda x: None if pd.isna(x) else x))
+    df = df.fillna("None")
 
     st.subheader("Data Summary")
     st.write("Total records uploaded: " + str(f"{len(df):,}"))
 
-    if st.button("Compare Locations"):
-        st.subheader("Filter Data")
-        columns = df.columns.to_list()
-        selected_column = st.selectbox("Select column to filter by", columns)
-        unique_values = df[selected_column].unique()
-        selected_value = st.selectbox("Select value", unique_values)
-
+    #if st.button("Compare Locations"):
+        # Apply reverse geocoding
+    df[['City', 'State', 'Country']] = df.apply(
+        lambda row: get_location(row['Latitude'], row['Longitude']), axis=1)
+    df = df.fillna("None")
+    st.subheader("Filter Data")
+    # Add 'All' option to the list
+    columns = df.columns.to_list()
+    selected_column = st.selectbox("Select column to filter by", columns)
+    unique_values = ['All'] + sorted(df[selected_column].unique())
+    selected_value = st.selectbox("Select value", unique_values)
+    
+    # Filter logic
+    if selected_value == 'All':
+        filtered_df = df
+    else:
         filtered_df = df[df[selected_column] == selected_value]
-        st.write(filtered_df)
+    st.write("Total records selected: " + str(f"{len(filtered_df):,}"))
+    st.write(filtered_df)
 else:
     st.write("Waiting on  file upload...")
